19ddc063561ccdcd054ebcc30976ab7dfdbd2270
Nhan Nguyen Trong <89365881+ntn145@users.noreply.github.com>
2022-08-07 18:48:14 +0700

    Initial Commit

---
+++ Day3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day3
+//
+//  Created by Khoai Alpha on 07/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day3/Day3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day3App.swift
+//  Day3
+//
+//  Created by Khoai Alpha on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
